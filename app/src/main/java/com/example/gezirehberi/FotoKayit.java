633f9742c6314dd91521bd779b99b2bcbbaea387
Öğrenci <ogrenci@profiloatl.meb.k12.tr>
2024-04-16 10:29:12 +0300

    faerbase

---
+++ app/src/main/java/com/example/gezirehberi/FotoKayit.java
@@ -0,0 +1,14 @@
+package com.example.gezirehberi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FotoKayit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_foto_kayit);
+    }
+}
---
+++ app/src/main/java/com/example/gezirehberi/kayit.java
@@ -0,0 +1,14 @@
+package com.example.gezirehberi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class kayit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kayit);
+    }
+}
